213a403bfe03cf0fade4ef0112d31ce86f78569d
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-16 18:49:50 +0100

    design static navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './css/index.css';
+import avatarImg from './images/image-avatar.png';
+import courosalImg from './images/image-product-1.jpg'
+
+
+const avatar = document.querySelector('.avatar');
+const imgCourosal = document.querySelector('.img-courosal');
+
+avatar.src = avatarImg;
+imgCourosal.src = courosalImg
+
