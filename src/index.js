5c812a97d290c02ce71ac19d6bbc9d81589fe50e
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-16 22:05:27 +0100

    milestone 1: build mobile page static

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './css/index.css';
 import './css/slideshow.css';
+import './css/headline.css';
+import './css/cart.css';
 import avatarImg from './images/image-avatar.png';
-import courosalImg from './images/image-product-1.jpg'
+import courosalImg from './images/image-product-1.jpg';
+import plusIcon from './images/icon-plus.svg';
+import minusIcon from './images/icon-minus.svg';
 
 
 const avatar = document.querySelector('.avatar');
 const imgCourosal = document.querySelector('.img-courosal');
+const plusIconEl = document.querySelector('.plus-icon');
+const minusIconEl = document.querySelector('.minus-icon');
 
 avatar.src = avatarImg;
-imgCourosal.src = courosalImg
+imgCourosal.src = courosalImg;
+plusIconEl.src = plusIcon;
+minusIconEl.src = minusIcon;
 
